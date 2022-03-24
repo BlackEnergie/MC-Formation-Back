fbab2bb6497b77f25a909f22def268d1d775e41c
Théo Perrin <the.perrin33@gmail.com>
2022-03-24 15:02:25 +0100

    Initialized Spring boot application

---
+++ src/main/java/com/mcformation/McFormationBackApplication.java
@@ -0,0 +1,13 @@
+package com.mcformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McFormationBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McFormationBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mcformation/McFormationBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcformation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McFormationBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
