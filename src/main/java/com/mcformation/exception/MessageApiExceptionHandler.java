839e9bfc209e27343c566f282ca46ca8cf58f258
Théo Perrin <the.perrin@outlook.fr>
2022-04-06 12:08:48 +0200

    Refacto ExceptionHandler

--- src/main/java/com/mcformation/exception/MessageApiExceptionHandler.java
+++ src/main/java/com/mcformation/exception/MessageApiExceptionHandler.java
@@ -7,13 +7,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import org.springframework.validation.ObjectError;
 
 @ControllerAdvice
-public class ExceptionHandler extends ResponseEntityExceptionHandler {
+public class MessageApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
@@ -28,6 +29,15 @@ public class ExceptionHandler extends ResponseEntityExceptionHandler {
         messageApi.setCode(HttpStatus.BAD_REQUEST.value());
         messageApi.setMessage(message.toString());
         return new ResponseEntity<>(messageApi, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    protected ResponseEntity<MessageApi> handleRunTimeException(RuntimeException ex) {
+        MessageApi messageApi = new MessageApi();
+        HttpStatus code = HttpStatus.INTERNAL_SERVER_ERROR;
+        messageApi.setCode(code.value());
+        messageApi.setMessage(ex.getMessage());
+        return new ResponseEntity<>(messageApi, code);
     }
 
 }
