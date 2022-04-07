04d0e26bd1d669216f8f776bb25c1e868752585c
HugoLoup <hugoloup.33@gmail.com>
2022-04-07 16:59:01 +0200

    fix conflits

--- src/main/java/com/mcformation/McFormationBackApplication.java
+++ src/main/java/com/mcformation/McFormationBackApplication.java
@@ -1,15 +1,19 @@
 package com.mcformation;
 
+import com.mcformation.repository.RoleRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class McFormationBackApplication {
 
+    @Autowired 
+    RoleRepository roleRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(McFormationBackApplication.class, args);
     }
-
-
 
 }
