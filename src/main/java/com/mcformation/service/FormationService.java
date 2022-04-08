d8c019500664538dc9b8f2a8db5b6b1a8356755a
maxencetauzin <maxence.tauzin@gmail.com>
2022-04-08 17:21:28 +0200

    Suppression import inutiles et commentaires

--- src/main/java/com/mcformation/service/FormationService.java
+++ src/main/java/com/mcformation/service/FormationService.java
@@ -1,7 +1,6 @@
 package com.mcformation.service;
 
 import java.util.List;
-import java.util.Optional;
 
 import com.mcformation.mapper.DemandeMapper;
 import com.mcformation.mapper.FormateurMapper;
@@ -15,11 +14,9 @@ import com.mcformation.model.database.Association;
 import com.mcformation.model.database.Demande;
 import com.mcformation.model.database.Formateur;
 import com.mcformation.model.database.Formation;
-import com.mcformation.model.database.Utilisateur;
 import com.mcformation.repository.AssociationRepository;
 import com.mcformation.repository.FormateurRepository;
 import com.mcformation.repository.FormationRepository;
-import com.mcformation.repository.UtilisateurRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -32,11 +29,8 @@ public class FormationService {
     private FormateurRepository formateurRepository;
     @Autowired
     private AssociationRepository associationRepository;
-    @Autowired
-    private UtilisateurRepository utilisateurRepository;
 
     public List<FormationApi> getAllFormations() {
-        //List<Formation> formationList = (List<Formation>) formationRepository.findAll();
         List<Formation> formationList = (List<Formation>) formationRepository.findTop5ByOrderByDateDesc();
         List<FormationApi> formationApiList = FormationMapper.INSTANCE.formationDaoListToFormationApiList(formationList);
         for (FormationApi formationApi : formationApiList) {
@@ -45,21 +39,11 @@ public class FormationService {
             List<FormateurApi>formateursApi=FormateurMapper.INSTANCE.formateurDaoListToFormateurApiList(formateurs);
             formationApi.setFormateurs(formateursApi);
             Demande demande = formation.getDemande();
-            //Long user_id= associationRepository.findByIdDemande(demande.getId());
             Association association=associationRepository.findByIdDemande(demande.getId());
             AssociationApi associationApi=UtilisateurMapper.INSTANCE.associationDaoToAssociationApi(association);
             DemandeApi demandeApi=DemandeMapper.INSTANCE.demandeDaoToDemandeApi(demande);
             demandeApi.setAssociation(associationApi);
             formationApi.setDemande(demandeApi);
-            /* Optional<Utilisateur> utilisateurOptional = utilisateurRepository.findById(user_id);
-            if(utilisateurOptional.isPresent()){
-                    Utilisateur utilisateur = utilisateurOptional.get();
-                    Association association = associationRepository.findByUtilisateur(utilisateur);
-                    AssociationApi associationApi=UtilisateurMapper.INSTANCE.associationDaoToAssociationApi(association);
-                    DemandeApi demandeApi=DemandeMapper.INSTANCE.demandeDaoToDemandeApi(demande);
-                    demandeApi.setAssociation(associationApi);
-                    formationApi.setDemande(demandeApi);
-            } */
         }
         return formationApiList;
     }
