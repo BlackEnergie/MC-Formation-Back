f22833205ba97424a823fa541fc51bf851bf707a
Théo Perrin <the.perrin@outlook.fr>
2022-05-11 14:20:18 +0200

    Affectation formation api

---
+++ src/main/java/com/mcformation/model/api/AffectationFormationApi.java
@@ -0,0 +1,23 @@
+package com.mcformation.model.api;
+
+public class AffectationFormationApi {
+
+    private String nomUtilisateur;
+    private Long idFormation;
+
+    public String getNomUtilisateur() {
+        return nomUtilisateur;
+    }
+
+    public void setNomUtilisateur(String nomUtilisateur) {
+        this.nomUtilisateur = nomUtilisateur;
+    }
+
+    public Long getIdFormation() {
+        return idFormation;
+    }
+
+    public void setIdFormation(Long idFormation) {
+        this.idFormation = idFormation;
+    }
+}
