583d112f7e49f08bfc5c2864d1f89977008205e2
Théo Perrin <the.perrin@outlook.fr>
2022-05-18 10:29:47 +0200

    modification model et tests

--- src/main/java/com/mcformation/utils/JsonUtils.java
+++ src/main/java/com/mcformation/utils/JsonUtils.java
@@ -2,22 +2,21 @@ package com.mcformation.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class JsonUtils {
 
+    static Logger logger = LoggerFactory.getLogger(JsonUtils.class);
     public static String objectToJson(Object object) {
-        String res = "parsing error";
+        String res = null;
         try {
             ObjectMapper ow = new ObjectMapper();
             res = ow.writeValueAsString(object);
         } catch (JsonProcessingException e) {
-            System.out.println(e.getMessage());
+            logger.error("Error parsing object to json", e);
         }
         return res;
-    }
-
-    public static void printObject(Object object) {
-        System.out.println(objectToJson(object));
     }
 
 }
